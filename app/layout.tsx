18d36c45dd855f72fd83d6ff42906ed58ae34a9c
webproformation <gregory.demeulenaere@gmail.com>
2026-02-11 15:58:37 +0100

    Version 1.0.0.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { LayoutWrapper } from '@/components/layout-wrapper';
 const pangolin = Pangolin({ weight: '400', subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'La Boutique de Morgane',
-  description: 'La boutique mode et lifestyle de Morgane',
+  title: 'KAVERN',
+  description: 'Des prix mini, des choix en or',
 };
 
 export default function RootLayout({
